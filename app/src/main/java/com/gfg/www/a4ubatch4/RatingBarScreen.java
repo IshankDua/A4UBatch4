8cf1fb648af4341fa764aaa511f1e8c4ff8d7791
Ishank Dua <ishank@github.com>
2020-08-06 20:00:07 +0530

    Rating Bar Added

---
+++ app/src/main/java/com/gfg/www/a4ubatch4/RatingBarScreen.java
@@ -0,0 +1,14 @@
+package com.gfg.www.a4ubatch4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RatingBarScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating_bar_screen);
+    }
+}
