c5d0fb07534ef50f50a8e5c2615fc6faf26d7e79
Ishank Dua <ishank@github.com>
2020-08-07 09:19:03 +0530

    Validation in login added

--- app/src/main/java/com/gfg/www/a4ubatch4/LoginScreen.java
+++ app/src/main/java/com/gfg/www/a4ubatch4/LoginScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginScreen extends AppCompatActivity {
 
     Button b1,b2;
+    EditText e1,e2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,13 +22,45 @@ public class LoginScreen extends AppCompatActivity {
 
         b1= (Button) findViewById(R.id.button8);
         b2= (Button) findViewById(R.id.button9);
+        e1= (EditText) findViewById(R.id.e1);
+        e2= (EditText) findViewById(R.id.e2);
 
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(LoginScreen.this, "Login SuccessFull", Toast.LENGTH_SHORT).show();
+                String username = "Ishan";
+                String password = "12345";
+
+
+
+                String inputusername = e1.getText().toString();
+                String inputPassword = e2.getText().toString();
+
+
+                if(inputusername.equals(username)){
+                    if(inputPassword.equals(password)){
+
+                        Toast.makeText(LoginScreen.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+
+                        Intent a = new Intent(LoginScreen.this,MainScreen.class);
+                        startActivity(a);
+                    }
+                    else{
+                        Toast.makeText(LoginScreen.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText(LoginScreen.this, "Incorrect Username", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+
+
             }
         });
 
