beafd5f18874a24a93449f39a931a9251d63f3dd
Ishank Dua <ishank@github.com>
2020-08-08 17:49:36 +0530

    Validation in login added

--- app/src/main/java/com/gfg/www/a4ubatch4/SignupScreen.java
+++ app/src/main/java/com/gfg/www/a4ubatch4/SignupScreen.java
@@ -1,15 +1,32 @@
 package com.gfg.www.a4ubatch4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupScreen extends AppCompatActivity {
 
+    TextView t1;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_screen);
+        t1 = (TextView) findViewById(R.id.textView14);
+
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(SignupScreen.this,LoginScreen.class);
+                startActivity(a);
+            }
+        });
     }
+
+
 }
